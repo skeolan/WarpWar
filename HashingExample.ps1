047ad94ef4dff57c743a83605512e3cadf702a30
Patrick Maughan <pfmaug@gmail.com>
2015-08-04 16:32:17 -0700

    More fluent hashing

--- HashingExample.ps1
+++ HashingExample.ps1
@@ -1,3 +1,12 @@
-$myHash = @{}
-("a=1/2 b=1/3 c=1tb d=2.5 e=1" -split " ") | % { $myHash.add( ($_ -split "=")[0], (Invoke-Expression ($_ -split "=")[1] )) }
-$myHash
+function HashFromSpec()
+{
+    [cmdletBinding()]
+    param(
+        [string]$hashSpec
+    )
+    $myHash = @{}
+    ($hashSpec -split " ") | % { $myHash.add( ($_ -split "=")[0], (Invoke-Expression ($_ -split "=")[1] )) }
+    $myHash
+}
+
+HashFromSpec -hashSpec "a=1/2 b=1/3 c=1tb d=2.5 e=1"
--- WarpWarSolver.ps1
+++ WarpWarSolver.ps1
@@ -1,10 +1,10 @@
-$bpCostSpec      = @{ PD=1; B=1; S=1; T=1; M=1/3; SR=1; C=1; SH=1/6; A=1/2; E=1; H=1/10; R=5; CP=15; SWG=3; MWG=5; LWG=10; SB=25 }
-$templateAttrSpec= "PD:0 B:0 S:0 T:0 M:0 SR:0 C:0 SH:0 A:0 E:0 H:0 R:0 CP:0 SWG:0 MWG:0 LWG:0 SB:0"
-$templateInfoSpec= "Name:TS1_-_Template_Ship Owner:Template_Owner Location:COORD[0,0] BPCost:0"
+$bpCostSpec      = "PD=1 B=1 S=1 T=1 M=1/3 SR=1 C=1 SH=1/6 A=1/2 E=1 H=1/10 R=5 CP=15 SWG=3 MWG=5 LWG=10 SB=25"
+$templateAttrSpec= "PD=0 B=0 S=0 T=0 M=0 SR=0 C=0 SH=0 A=0 E=0 H=0 R=0 CP=0 SWG=0 MWG=0 LWG=0 SB=0"
+$templateInfoSpec= "Name=TS1_-_Template_Ship Owner=Template_Owner Location=COORD[0,0] BPCost=0"
 $templateSpec    = "{0} -- {1}" -f $templateInfoSpec, $templateAttrSpec
 
-$WS1Spec    = "Name:WS1_-_Gladia_001 Owner:Empire Location:COORD[1,1] -- SWG:1 PD:4 B:2 S:1"
-$WS2Spec    = "Name:WS2_-_Vulpis_001 Owner:Rebels Location:COORD[2,2] -- SWG:1 PD:4 T:1 S:1 M:3"
+$WS1Spec    = "Name=WS1_-_Gladia_001 Owner=Empire Location=COORD[1,1] -- SWG=1 PD=4 B=2 S=1"
+$WS2Spec    = "Name=WS2_-_Vulpis_001 Owner=Rebels Location=COORD[2,2] -- SWG=1 PD=4 T=1 S=1 M=3"
 
 function main()
 {
@@ -12,15 +12,20 @@ function main()
     param(
         [string[]]$argList
     )
-    $shipTemplate    = loadShip    -spec     $templateSpec
+    $shipTemplate = loadShip -spec $templateSpec
     
     $WSSpecs     = @($WS1Spec, $WS2Spec)
     $shipObjects = @()
 
-    $WSSpecs | % { $newShip = loadShip -spec $_ -template $shipTemplate -costSpec $bpCostSpec; $shipObjects += @($newShip); }
+    $bpCosts = @{}
+    (HashFromSpec -hashSpec $bpCostSpec).GetEnumerator() | % { $bpCosts.Add($_.Key, (Invoke-Expression $_.Value)) }
 
-    WriteSummary     -verboseSet @($shipTemplate) -shipSet $shipObjects
-    WriteCostSummary -costSpec $bpCostSpec | % { write-debug $_ }
+    write-verbose $bpCosts
+
+    $WSSpecs | % { $newShip = loadShip -spec $_ -template $shipTemplate -costSpec $bpCosts; $shipObjects += @($newShip); }
+
+    WriteSummary     -verboseSet (@($shipTemplate)+$shipObjects) -shipSet $shipObjects
+    WriteCostSummary -costSpec $bpCosts | % { $_ }
     return
 }
 
@@ -31,13 +36,13 @@ function loadShip($spec, $template, $costSpec)
     write-debug ("
     Loading spec  [{0}] starting from [{1}]" -f $spec, $template.Name)
 
-    #clone template ship info and attributes
-    $ship         = ($template,       @{} -ne $null)[0].Clone()
-    $ship["attrs"]= ($template.attrs, @{} -ne $null)[0].Clone() 
-    
     $infoSpec, $attrSpec = ($spec -split " -- ")
-    $infoSpec -split " " | % { $k, $v = $_ -split ":"; $ship[      "$k"]=$v.Replace("_", " "); }
-    $attrSpec -split " " | % { $k, $v = $_ -split ":"; $ship.attrs["$k"]=$v; }
+    $ship = HashFromSpec -hashSpec $infoSpec
+    $ship["attrs"]= HashFromSpec -hashSpec $attrSpec
+
+    #Post-Process
+    #foreach $k, $v in $ship, $ship["$k"]=$v.Replace("_", " "); }
+    #Reinsert zero-value template attributes unless overridden by spec -- "sum" the two hashes?
 
     $ship.BPCost = calcBPCost -attrSet $ship.attrs -costSpec $costSpec
 
@@ -47,6 +52,17 @@ function loadShip($spec, $template, $costSpec)
 function calcBPCost ($attrSet, $costSpec)
 {
     ($attrSet.GetEnumerator() | ? { $_.Value -ne 0 } | % { [decimal]::Ceiling( [decimal]$_.Value * [decimal]$costSpec[$_.Key] ); } | measure-object -sum).Sum
+}
+
+function HashFromSpec()
+{
+    [cmdletBinding()]
+    param(
+        [string]$hashSpec
+    )
+    $myHash = @{}
+    ($hashSpec -split " ") | % { $myHash.add( ($_ -split "=")[0], ($_ -split "=")[1] ) }
+    $myHash
 }
 
 
@@ -95,7 +111,7 @@ function WriteCostSummary($costSpec)
 {
     ""
     '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-    $costSpec
+    $costSpec.GetEnumerator() | % { "{0,-20} costs `${1}" -f $_.Key, $_.Value }
     '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     ""
 }
