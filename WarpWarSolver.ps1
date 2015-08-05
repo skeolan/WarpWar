aea21a93a8f4d00327314d5a1594cc2c84719cfc
Patrick Maughan <pfmaug@gmail.com>
2015-08-04 17:49:41 -0700

    More fluent hashing and numeric vs text handling

--- WarpWarSolver.ps1
+++ WarpWarSolver.ps1
@@ -1,6 +1,6 @@
 $bpCostSpec      = "PD=1 B=1 S=1 T=1 M=1/3 SR=1 C=1 SH=1/6 A=1/2 E=1 H=1/10 R=5 CP=15 SWG=3 MWG=5 LWG=10 SB=25"
 $templateAttrSpec= "PD=0 B=0 S=0 T=0 M=0 SR=0 C=0 SH=0 A=0 E=0 H=0 R=0 CP=0 SWG=0 MWG=0 LWG=0 SB=0"
-$templateInfoSpec= "Name=TS1_-_Template_Ship Owner=Template_Owner Location=COORD[0,0] BPCost=0"
+$templateInfoSpec= "Name=TS1_-_Template_Ship Owner=Template_Owner Location=COORD[0,0] BPCost=0 Universe=Reign_Of_Stars"
 $templateSpec    = "{0} -- {1}" -f $templateInfoSpec, $templateAttrSpec
 
 $WS1Spec    = "Name=WS1_-_Gladia_001 Owner=Empire Location=COORD[1,1] -- SWG=1 PD=4 B=2 S=1"
@@ -12,16 +12,16 @@ function main()
     param(
         [string[]]$argList
     )
-    $shipTemplate = loadShip -spec $templateSpec
-    
     $WSSpecs     = @($WS1Spec, $WS2Spec)
     $shipObjects = @()
 
-    $bpCosts = @{}
-    (HashFromSpec -hashSpec $bpCostSpec).GetEnumerator() | % { $bpCosts.Add($_.Key, (Invoke-Expression $_.Value)) }
+    "Loading numeric cost spec..."
+    $bpCosts = HashFromSpec -hashSpec $bpCostSpec -numeric
 
-    write-verbose $bpCosts
-
+    "Loading ship template..."
+    $shipTemplate = loadShip -spec $templateSpec
+    
+    "Loading real ships..."
     $WSSpecs | % { $newShip = loadShip -spec $_ -template $shipTemplate -costSpec $bpCosts; $shipObjects += @($newShip); }
 
     WriteSummary     -verboseSet (@($shipTemplate)+$shipObjects) -shipSet $shipObjects
@@ -33,18 +33,19 @@ function main()
 
 function loadShip($spec, $template, $costSpec)
 {
-    write-debug ("
-    Loading spec  [{0}] starting from [{1}]" -f $spec, $template.Name)
 
     $infoSpec, $attrSpec = ($spec -split " -- ")
+    Write-Host " -- Loading text info ..."
     $ship = HashFromSpec -hashSpec $infoSpec
-    $ship["attrs"]= HashFromSpec -hashSpec $attrSpec
+    Write-Host " -- Loading numeric attributes ..."
+    $ship["attrs"]= HashFromSpec -hashSpec $attrSpec -numeric
 
     #Post-Process
-    #foreach $k, $v in $ship, $ship["$k"]=$v.Replace("_", " "); }
-    #Reinsert zero-value template attributes unless overridden by spec -- "sum" the two hashes?
-
-    $ship.BPCost = calcBPCost -attrSet $ship.attrs -costSpec $costSpec
+    # -- Instantiate all templated values not overridden by spec
+      if($template -ne $null) { $template.GetEnumerator() | ? {$_.Key -ne "attrs" } | % { if (-not $ship.ContainsKey($_.Key)) {$ship.add($_.Key, $_.Value) } } }
+      if($template -ne $null) { $template.attrs.GetEnumerator() | % { if (-not $ship.attrs.ContainsKey($_.Key)) {$ship.attrs.add($_.Key, $_.Value) } } }
+    # -- Calculate BP cost based on cost spec and unit attributes
+      $ship.BPCost = calcBPCost -attrSet $ship.attrs -costSpec $costSpec    
 
     return $ship
 }
@@ -58,10 +59,17 @@ function HashFromSpec()
 {
     [cmdletBinding()]
     param(
-        [string]$hashSpec
+        [string]$hashSpec,
+        [switch]$numeric
     )
     $myHash = @{}
-    ($hashSpec -split " ") | % { $myHash.add( ($_ -split "=")[0], ($_ -split "=")[1] ) }
+    ($hashSpec -split " ") | foreach {
+        $myKey=($_ -split "=")[0]
+        $myVal=($_ -split "=")[1]
+        if($numeric) { $myVal=Invoke-Expression $myVal }
+        else         { $myVal=$myVal -replace "_", " " }
+        $myHash.add($myKey, $myVal)
+    }
     $myHash
 }
 
@@ -110,9 +118,9 @@ function WriteSummary($verboseSet, $shipSet)
 function WriteCostSummary($costSpec)
 {
     ""
-    '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+    '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     $costSpec.GetEnumerator() | % { "{0,-20} costs `${1}" -f $_.Key, $_.Value }
-    '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+    '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     ""
 }
 
